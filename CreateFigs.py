dbb196547442a52e36262c252cfa671bb40730ef
bellab4710 <bellab4710@email.arizona.edu>
2021-05-26 09:41:17 -0700

    Updated to Make Second Figure

--- CreateFigs.py
+++ CreateFigs.py
@@ -5,6 +5,11 @@ from matplotlib import pyplot as plt
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-junk_data = pd.read_csv("./junkData.csv")
-sns.lineplot(x = "M/N", y = "SNR, dB", data = junk_data, hue = "Algorithm")
+fig1_data = pd.read_csv("./junkData.csv")
+#Upper left in Carrillo Paper
+sns.lineplot(x = "M/N", y = "SNR, dB", data = fig1_data, hue = "Algorithm")
+plt.show()
+fig2_data = pd.read_csv("./fig2.csv")
+#Upper right
+sns.lineplot(x = "M/N", y = "Time, sec", data = fig2_data, hue = "Algorithm", markers = True)
 plt.show()
